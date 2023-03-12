9c72224ae6946ea706bb13431f52341661bc17ef
Teddystone08 <ernestlrty@hotmail.com>
2023-03-12 14:46:14 +0000

    insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,24 +7,18 @@
  * Return: NULL if it fails or the new node
  */
 
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
-
 	if (parent == NULL)
-	{
 		return (NULL);
-	}
+	binary_tree_t *new_node = binary_tree_node(parent, value);
 
-	*new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
-	{
 		return (NULL);
-	}
 	if (parent->left != NULL)
 	{
 		new_node->left = parent->left;
-		parent->right->parent = new_node;
+		parent->left->parent = new_node;
 	}
 	parent->left = new_node;
 	return (new_node);
